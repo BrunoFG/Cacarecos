fc4b724a2b93926ab951070c29d94abdbce914bd
Bruno França Guimarães <correio.bfg@gmail.com>
2023-08-28 12:46:02 -0300

    Update diaAniversario.py
    Generalização

--- Python_Matematica_Discreta/diaAniversario.py
+++ Python_Matematica_Discreta/diaAniversario.py
@@ -1,6 +1,7 @@
 respostas = []
-for a in range(5):
-    for i in range(1, 32):
+n=5
+for a in range(n):
+    for i in range(1, pow(2, n)):
         if i%(2**(a+1))>=2**a:
             print(i, end=' ')
     respostas.append(int(input("\nO dia do seu aniversário está aqui? (1 para sim e 0 para não)\n")))
